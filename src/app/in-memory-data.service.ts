cb705c3fcea8e258cfe2d8f1ba81b37f06e65f49
Santhosh12072000 <santhose12072000@gmail.com>
2022-04-15 19:11:02 +0530

    3 files modified

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,16 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 1, name: 'Tata Steel' },
-      { id: 2, name: 'Reliance' },
-      { id: 3, name: 'TCS' },
-      { id: 4, name: 'Adani Power' },
-      { id: 5, name: 'HDFC' },
-      { id: 6, name: 'Hindustan Unilever Limited' },
-      { id: 7, name: 'Nestle' },
-      { id: 8, name: 'Infosys' },
-      { id: 9, name: 'ITC' },
-      { id: 10, name: 'Wipro' }
+      { id: 1, name: 'Tata Steel',price: 1345 },
+      { id: 2, name: 'Reliance',price: 2250 },
+      { id: 3, name: 'TCS',price: 3850 },
+      { id: 4, name: 'Adani Power',price: 420 },
+      { id: 5, name: 'HDFC',price: 1445 },
+      { id: 6, name: 'Hindustan Unilever Limited',price: 2152 },
+      { id: 7, name: 'Nestle',price: 18382 },
+      { id: 8, name: 'Infosys',price: 1700 },
+      { id: 9, name: 'ITC',price: 252 },
+      { id: 10, name: 'Wipro',price: 650 }
     ];
     return {heroes};
   }
